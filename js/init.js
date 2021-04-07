958a785c602fe9ac5b67ab585dca0fc3ebdaa717
aut41ejemplo <82073773+aut41ejemplo@users.noreply.github.com>
2021-04-06 23:42:39 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,15 +2,11 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
- firebase.initializeApp({
-   apiKey: "AIzaSyBn3cY4ZvG8Yqp3LTtUKtQztgd2N62nQ_k",
-    authDomain: "autentifica4.firebaseapp.com",
-    projectId: "autentifica4",
-    storageBucket: "autentifica4.appspot.com",
-    messagingSenderId: "393482636225",
-    appId: "1:393482636225:web:dda7e212508288d0c4fd40",
-    measurementId: "G-2GJDC4PY3D"
-  });
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+firebase.initializeApp({
+       apiKey: "AIzaSyD6hGYyy1ujzD5EcSK3A9n8X-22O2Pdtfg",
+    authDomain: "autentifica-2f286.firebaseapp.com",
+    projectId: "autentifica-2f286",
+    storageBucket: "autentifica-2f286.appspot.com",
+    messagingSenderId: "881373419222",
+    appId: "1:881373419222:web:e6516672994e6dcc945b77"
+    });
