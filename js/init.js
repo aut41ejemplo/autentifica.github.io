5aa5cb5cafe3b60e5eb1523677620a7aa05059b0
aut41ejemplo <82073773+aut41ejemplo@users.noreply.github.com>
2021-04-06 23:05:57 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,15 +2,15 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
- var firebaseConfig = {
-    apiKey: "AIzaSyBn3cY4ZvG8Yqp3LTtUKtQztgd2N62nQ_k",
+ firebase.initializeApp({
+   apiKey: "AIzaSyBn3cY4ZvG8Yqp3LTtUKtQztgd2N62nQ_k",
     authDomain: "autentifica4.firebaseapp.com",
     projectId: "autentifica4",
     storageBucket: "autentifica4.appspot.com",
     messagingSenderId: "393482636225",
     appId: "1:393482636225:web:dda7e212508288d0c4fd40",
     measurementId: "G-2GJDC4PY3D"
-  };
+  });
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
